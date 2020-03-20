e6259800c19af9c1a28297c246c2a7005b892454
Michael Nieto <mikeand9@gmail.com>
2020-03-19 20:42:52 -0500

    init project

---
+++ services/src/main/kotlin/club/piggyplanner/services/ServicesApplication.kt
@@ -0,0 +1,11 @@
+package club.piggyplanner.services
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServicesApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServicesApplication>(*args)
+}
